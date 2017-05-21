249c0e9df69ab5e4cd1f66a6ed0e938101480a1a
Sebastian Godoy <sego.du@hotmail.com>
2017-05-20 23:10:09 -0500

    Update Main.cpp

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -38,8 +38,8 @@
 #ifdef _WIN32
 #include "ServiceWin32.h"
 char serviceName[] = "worldserver";
-char serviceLongName[] = "TrinityCore world service";
-char serviceDescription[] = "TrinityCore World of Warcraft emulator world service";
+char serviceLongName[] = "AsmodeosCore world service";
+char serviceDescription[] = "AsmodeosCore World of Warcraft emulator world service";
 /*
  * -1 - not in service mode
  *  0 - stopped
